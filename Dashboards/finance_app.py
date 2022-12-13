dc202384b1a373910f2fd27691d9cac176c5f571
Ian Rossi <ian.rossi08@hotmail.com>
2022-12-13 08:34:22 -0300

    Adicionado novo gráfico de dividendos

--- Dashboards/finance_app.py
+++ Dashboards/finance_app.py
@@ -23,36 +23,43 @@ app.layout = html.Div([
         dcc.Input(
             id='input-stock',
             placeholder='Ex: APPL',
-            type='text',
-            value='APPL'),
+            type='text'),
         html.Button(id='submit-button', n_clicks=0, children='Submit')
     ], style={'width': '48%', 'textAlign': 'left', 'justifyContent':'center'}
     ),
     html.Hr(),
-    dcc.Graph(id='graphic')
+    dcc.Graph(id='graphic-candlestick'),
+    dcc.Graph(id='graphic-dividends')
     
 ])
 
 @app.callback(
-    Output('graphic', 'figure'),
+    [Output('graphic-candlestick', 'figure'),
+    Output('graphic-dividends', 'figure')],
     Input('submit-button', 'n_clicks'),
     State('input-stock', 'value')
 )
+
 def update_graph(n_clicks, stock):
 
     ticker = yf.Ticker(str(stock.upper()))
     df = ticker.history(period='max')
     df = df.reset_index()
     
-    fig = go.Figure(data=[go.Candlestick(x=df['Date'],
+    df_dividends = ticker.dividends.to_frame().reset_index()
+    df_dividends['Date'] = df_dividends['Date'].dt.date
+    
+    fig_candlestick = go.Figure(data=[go.Candlestick(x=df['Date'],
                 open=df['Open'],
                 high=df['High'],
                 low=df['Low'],
                 close=df['Close'])])
             
-    fig.update_layout(transition_duration=500, xaxis_rangeslider_visible=False)
+    fig_candlestick.update_layout(transition_duration=500, xaxis_rangeslider_visible=False)
     
-    return fig
+    fig_dividends = px.bar(df_dividends, x='Date', y='Dividends')
+    
+    return fig_candlestick, fig_dividends
 
 
 if __name__ == '__main__':
